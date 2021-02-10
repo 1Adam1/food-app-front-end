5b8135b4a475a82333ef622d886507c693958f1b
Adam Wasilewski <brzoza10k@gmail.com>
2021-02-10 21:32:32 +0100

    Style products list

--- food-app/src/app/components/product/product-list/product-list.component.ts
+++ food-app/src/app/components/product/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/model/interfaces/product.interface';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,14 +9,28 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+  pageLoaded = false;
+  products: Product[] = [];
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) { }
 
   ngOnInit(): void {
+    this.productService.getAllProducts().subscribe(results => {
+      this.products = results;
+      this.pageLoaded = true;
+    });
   }
 
   goToProductCreationPage() {
     this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  goToProductInfoPage(product: Product) {
+    this.router.navigate([product._id], {relativeTo: this.route});
   }
 
 }
