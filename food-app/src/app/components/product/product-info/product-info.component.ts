4db52587e1aef95eb36ae33b1cc92c7d0c538211
Adam Wasilewski <brzoza10k@gmail.com>
2021-02-10 21:49:57 +0100

    Add product info

--- food-app/src/app/components/product/product-info/product-info.component.ts
+++ food-app/src/app/components/product/product-info/product-info.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/model/interfaces/product.interface';
+import { ProductService } from 'src/app/services/product.service';
+import { DataInfoComponentInputFormat } from '../../data-info/data-info.component';
 
 @Component({
   selector: 'app-product-info',
@@ -6,10 +10,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-info.component.scss']
 })
 export class ProductInfoComponent implements OnInit {
+  pageLoaded = false;
+  product: Product;
+  data: DataInfoComponentInputFormat;
 
-  constructor() { }
+  constructor(
+    private productService: ProductService, 
+    private router: Router, 
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const productId = this.route.snapshot.params['id'];
+
+    this.productService.getProduct(productId).subscribe(result => {
+      this.product = result;
+      this.data = this.prepareDataInfoComponentInputFormat('Product data');
+      this.pageLoaded = true;
+    });
+  }
+
+  private prepareDataInfoComponentInputFormat(header: string): DataInfoComponentInputFormat {
+    const result: DataInfoComponentInputFormat = {
+      header,
+      values: [
+        {
+          key: 'Name',
+          value: this.product.name
+        },
+        {
+          key: 'Kilocalories per unit',
+          value: this.product.kilocaloriesPerUnit
+        },
+        {
+          key: 'Default unit',
+          value: this.product.defaultUnit
+        },
+        {
+          key: 'Description',
+          value: this.product.description
+        }
+      ]
+    };
+
+    return result;
+  }
+
+  goToProductEditionPage() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
 }
